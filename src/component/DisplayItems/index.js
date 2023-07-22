f90173cd0d0007de22d829bb52a6831d48b7cd3a
lekhya11 <lekhya.kondaveeti@gmail.com>
2023-07-22 19:20:33 +0530

    made changes in display items

--- src/component/DisplayItems/index.js
+++ src/component/DisplayItems/index.js
@@ -1,11 +1,12 @@
 import "./index.css"
 
 const DisplayItems = (props) => {
-    const {Item} = props
+    const {Item} = props //getting the props 
     console.log(Item)
     return (
         <li>
-            <h1 className="items">{Item}</h1>
+            <h1 className="items">{Item}</h1> 
+           
         </li>
     )
 }
