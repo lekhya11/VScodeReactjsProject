aa5997e06f67528c99314336d6db6034cfba9821
lekhya11 <lekhya.kondaveeti@gmail.com>
2023-07-25 21:02:47 +0530

    adding delete and star to the list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import {Component} from "react"
+import {v4 as uuidv4} from 'uuid'
 import "./App.css"
 
 import DisplayItems from "./component/DisplayItems"
 
 const dataList = [
-  "State","Props","Updating the state","Adding new date to the list","Changing the text based on the state or event"
+  {
+    id: uuidv4(),
+    Topic: "Props and State",
+    isCompleted: false
+  },
+   {
+    id: uuidv4(),
+    Topic: "class component and function component",
+    isCompleted: false
+  },
+  {
+    id: uuidv4(),
+    Topic: "Adding new data to the list",
+    isCompleted: false
+  }, 
+  {
+    id: uuidv4(),
+    Topic: "Props",
+    isCompleted: false
+  },
+
 ]
 
 class BTSWorld extends Component {
@@ -16,13 +37,41 @@ class BTSWorld extends Component {
   onChangeInput = (event) => {
     this.setState({inputValue: event.target.value}) // updating the input values on change
   }
+
+  onClicStarImage = (id) => {
+    const {listItems} = this.state
+    // iterating in each object in te list and using if statemnet to check the each.id is same as id and updating the isComplete value.
+    const updatedList = listItems.map(each => {
+      if (each.id === id) {
+        return {...each, isCompleted: !each.isCompleted}
+      }
+      //return the rest as it is 
+      return each
+     })
+
+     this.setState({listItems:updatedList})
+  }
   
   onAddButton = () => {
     const {inputValue} = this.state
-    this.setState(prevState => ({listItems: [...prevState.listItems, inputValue ], //adding new date to the list using spread operator
+    const newDetails = {
+      id: uuidv4(),
+      Topic:inputValue,
+      isCompleted:false
+    }
+
+    this.setState(prevState => ({listItems: [...prevState.listItems, newDetails], //adding new date to the list using spread operator
       inputValue:" " //clear the input filed after adding the values
     }))
     
+  }
+
+  deleteList = (id) => {
+     const {listItems} = this.state
+     //console.log(id)
+     //filter method give condition
+     const filterList = listItems.filter(each => each.id !== id)
+     this.setState({listItems:filterList})
   }
 
   render () {
@@ -38,11 +87,13 @@ class BTSWorld extends Component {
         />
         <button className="button" onClick={this.onAddButton}>ADD</button>
         </div>
+        { listItems.length === 0 ? <p>You have not planned yet </p> :
         <ul >
           {listItems.map(each => 
             //passing the props to the component
-            <DisplayItems Item={each} key={each} />)}
+            <DisplayItems Item={each} key={each} onClicStarImage={this.onClicStarImage} deleteList={this.deleteList}/>)}
         </ul>
+       }
       </div>
     )
   }
--- src/component/DisplayItems/index.js
+++ src/component/DisplayItems/index.js
@@ -1,12 +1,31 @@
 import "./index.css"
 
 const DisplayItems = (props) => {
-    const {Item} = props //getting the props 
+    const {Item,onClicStarImage,deleteList} = props //getting the props 
+    const {Topic, isCompleted,id} = Item
     console.log(Item)
+
+    const starImgUrl = isCompleted
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+    const ClickedStar = () => {
+        onClicStarImage(id)
+    }
+
+    const onClickDelete = () => {
+        deleteList(id)
+    }
+
     return (
         <li>
-            <h1 className="items">{Item}</h1> 
-           
+            <h1 className="items">{Topic}</h1> 
+            <div>
+            <img src={starImgUrl} alt="star" onClick={ClickedStar} />     
+            <button className="deleteButton" onClick ={onClickDelete}>
+                <img src="https://assets.ccbp.in/frontend/react-js/delete-img.png" alt="delete" />
+            </button>
+            </div>
         </li>
     )
 }
